7bb550c78044d44de7e2f24abc0ebb7d7045263e
Christopher Mendes <77082167+bos21001@users.noreply.github.com>
2023-03-11 19:09:44 -0300

    feat: migrations generated
    It creates the following tables:
    
    - finances
    - finances_historys
    - groups
    - group_users
    
    refs: #8

--- app/Containers/AppSection/Finance/Data/Migrations/2023_03_07_041756_create_finances_table.php
+++ app/Containers/AppSection/Finance/Data/Migrations/2023_03_07_041756_create_finances_table.php
@@ -12,9 +12,42 @@ return new class extends Migration {
     {
         Schema::create('finances', function (Blueprint $table) {
             $table->id();
-
+            $table->integer('user_id');
+            $table->integer('group_id')->nullable();
+            $table->string('name');
+            $table->float('value');
+            $table->string('description')->nullable();
+            $table->boolean('repeats');
+            $table->boolean('business_day_only');
+            $table->integer('repeat_every')->nullable();
+            $table->string('repetition_period')->nullable();
+            $table->timestamp('ends')->nullable();
             $table->timestamps();
-            //$table->softDeletes();
+        });
+
+        Schema::create('finance_historys', function (Blueprint $table) {
+            $table->id();
+            $table->integer('finance_id');
+            $table->integer('user_id');
+            $table->float('value');
+            $table->timestamp('date');
+            $table->string('action');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('name');
+            $table->integer('description')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('group_users', function (Blueprint $table) {
+            $table->integer('group_id');
+            $table->integer('user_id');
+            $table->timestamps();
         });
     }
 
@@ -24,5 +57,8 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::dropIfExists('finances');
+        Schema::dropIfExists('finance_historys');
+        Schema::dropIfExists('groups');
+        Schema::dropIfExists('group_has_users');
     }
 };
