ba5581cbb25ec3871705552edb873453cd3a3603
Christopher Mendes <77082167+bos21001@users.noreply.github.com>
2023-03-12 03:05:50 -0300

    feat(api): enrich CreateFinance endpoint response JSON
    The JSON returned by the CreateFinance API endpoint has been enriched to include additional fields that provide more detailed information about the created finance transaction. This includes the object type, ID, group ID, name, type, value, description, whether it repeats, whether it's business day only, the repetition frequency, repetition period, end date, and a human-readable created at timestamp.
    
    Refs #8

--- app/Containers/AppSection/Finance/UI/API/Transformers/FinanceTransformer.php
+++ app/Containers/AppSection/Finance/UI/API/Transformers/FinanceTransformer.php
@@ -20,15 +20,24 @@ class FinanceTransformer extends ParentTransformer
         $response = [
             'object' => $finance->getResourceKey(),
             'id' => $finance->getHashedKey(),
+            'group_id' => $finance->group_id,
+            'name' => $finance->name,
+            'type' => $finance->type,
+            'value' => $finance->value,
+            'description' => $finance->description,
+            'repeats' => $finance->repeats,
+            'business_day_only' => $finance->business_day_only,
+            'repeat_every' => $finance->repeat_every,
+            'repetition_period' => $finance->repetition_period,
+            'ends' => $finance->ends,
+            'readable_created_at' => $finance->created_at->diffForHumans(),
         ];
 
         return $this->ifAdmin([
             'real_id' => $finance->id,
             'created_at' => $finance->created_at,
             'updated_at' => $finance->updated_at,
-            'readable_created_at' => $finance->created_at->diffForHumans(),
             'readable_updated_at' => $finance->updated_at->diffForHumans(),
-            // 'deleted_at' => $finance->deleted_at,
         ], $response);
     }
 }
